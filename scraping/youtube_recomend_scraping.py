46e944a679326aa16af2fafeba52d3747cc0ef36
Pablo Clemente <paclema@gmail.com>
2016-05-10 17:36:42 +0200

    update

--- scraping/youtube_recomend_scraping.py
+++ scraping/youtube_recomend_scraping.py
@@ -1,45 +1,28 @@
-# -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-url = "http://www.youtube.com/watch?v=QtXby3twMmI"
+root_url = 'http://youtube.com'
 
-# Realizamos la peticion a la web
-req = requests.get(url)
+def get_video_page_urls():
+    #Sending the http request
+    response = requests.get('https://www.youtube.com/watch?v=QtXby3twMmI')
+    # making the soup! yummy ;)
+    soup = BeautifulSoup(response.text, "html.parser")
+    video_page_urls =  [a.attrs.get('href') for a in soup.select('ul.video-list a[href^=/watch]')]
+    return video_page_urls
 
-# Comprobamos que la petición nos devuelve un Status Code = 200
-statusCode = req.status_code
-if statusCode == 200:
+def get_video_data(video_page_url):
+    video_data = {}
+    response = requests.get(root_url + video_page_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    video_data['url'] =  root_url + video_page_url
+    video_data['title'] =  soup.select('span#eow-title')[0].get_text()
+    video_data['visitas'] = soup.select('div.watch-view-count')[0].get_text()
+    return video_data
 
-    # Pasamos el contenido HTML de la web a un objeto BeautifulSoup()
-    html = BeautifulSoup(req.text,"html.parser")
-    #print soup.prettify()
+def show_video_stats():
+    video_page_urls = get_video_page_urls()
+    for video_page_url in video_page_urls:
+        print get_video_data(video_page_url)
 
-
-    # Obtenemos todos los divs donde estan las entradas
-    related_videos = html.find_all("div",class_="content-wrapper")
-    #print related_videos
-    #print type(related_videos)
-    #print related_videos[0]
-
-
-    for element in related_videos:
-        date = element.find(class_="title").get_text()
-
-    """
-    for i,related_video in enumerate(related_videos):
-
-        #content_wrapper = related_video.find('div', {'class' : 'content-wrapper'})
-        # Recorremos todas las entradas para extraer el título, autor y fecha
-        #for i,content_wrapper in enumerate(content_wrappers):
-            # Con el método "getText()" no nos devuelve el HTML
-        titulo = related_video.find('div', {'class' : 'tittle'}).getText()
-        titulo = related_video.find('span', {'class' : 'tittle'}).getText()
-            # Sino llamamos al método "getText()" nos devuelve también el HTML
-        visualizaciones = related_video.find('span', {'class' : 'view-count'}).getText()
-
-            # Imprimo el Título, Autor y Fecha de las entradas
-        print "%d - %s  |  %s " %(i+1,titulo,visualizaciones)
-    """
-else:
-    print "Status Code %d" %statusCode
+print(show_video_stats())
