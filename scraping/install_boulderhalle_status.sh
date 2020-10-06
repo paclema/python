cd0a96aea1ae818ed758929c7c3005e41715a9b1
Pablo Clemente Maseda <paclema@gmail.com>
2020-10-06 11:16:49 +0200

    Update service installation script

--- scraping/install_boulderhalle_status.sh
+++ scraping/install_boulderhalle_status.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Configure ddns config file:
 servicename="boulderhalle_status"
 servicefile="/lib/systemd/system/boulderhalle_status.service"
-servicescrip="/home/pi/seleniumInstall/python/scraping/boulderhalle_status.py"
+servicescrip="$SCRIPTPATH/boulderhalle_status.py"
+
 
 cat >> $servicefile <<- EOM
 [Unit]
 Description="$servicename"
-After=multi-user.target
-Conflicts=getty@tty1.service
+#After=multi-user.target
 
 [Service]
 Type=simple
+WorkingDirectory=$SCRIPTPATH
+User=pi
 ExecStart=/usr/bin/python3 "$servicescrip"
-StandardInput=tty-force
+Restart=always
 
 [Install]
 WantedBy=multi-user.target
